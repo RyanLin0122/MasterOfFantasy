97d8f58c87278cd80365a2f8e1fdff16c7a6119b
ryan.lin.hex@gmail.com <ryan.lin.hex@gmail.com>
2021-09-10 00:08:25 +0800

    Add Try On Func 50%

--- MOFServer/MOFServer/Handler/CashShopHandler.cs
+++ MOFServer/MOFServer/Handler/CashShopHandler.cs
@@ -33,18 +33,33 @@ public class CashShopHandler : GameHandler
     public void ProcessBuyEvent(CashShopRequest req, ServerSession session)
     {
         int ItemNum = req.Cata.Count;
+
         //判斷數量
         if (req.ID.Count != ItemNum || req.Tag.Count != ItemNum || req.Amount.Count != ItemNum)
         {
             //數量對不起來
             SendErrorBack(2, session);
+            return;
         }
+        //驗證總價
+        long TotalPrice = 0;
+        for (int i = 0; i < ItemNum; i++)
+        {
+            TotalPrice += CacheSvc.Instance.CashShopDic[req.Cata[i]][req.Tag[i]][0].SellPrice * req.Amount[i];
+        }
+        if (TotalPrice != req.TotalPrice)
+        {
+            SendErrorBack(3, session);
+            return;
+        }
+        List<Item> ItemList = new List<Item>();
         for (int i = 0; i < ItemNum; i++)
         {
             int ItemID = req.ID[i];
             int Amount = req.Amount[i];
             Item item = Utility.GetItemCopyByID(ItemID);
             item.Count = Amount;
+            ItemList.Add(item);
         }
 
     }
@@ -70,6 +85,8 @@ public class CashShopHandler : GameHandler
                 return "CashShopReq 為空";
             case 2:
                 return "數量錯誤";
+            case 3:
+                return "總價錯誤";
             default:
                 return "錯誤";
         }
