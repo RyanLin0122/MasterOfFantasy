4c1916ca584c0ce088acd68027e17399bbe7b98b
ryan.lin.hex@gmail.com <ryan.lin.hex@gmail.com>
2021-10-27 15:34:35 +0800

    Fix tooltip boundary

--- Assets/Scripts/Utility/DragWindow.cs
+++ Assets/Scripts/Utility/DragWindow.cs
@@ -3,20 +3,46 @@ using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
 
-public class DragWindow : MonoBehaviour{
+public class DragWindow : MonoBehaviour
+{
     [Header("滑鼠位置")]
     public Vector3 Prepos;             // 儲存滑鼠前一刻的位置
 
-    public void OnMouseDown(){
-        Prepos = Input.mousePosition ;  //按下時先儲存初始位置
+    public void OnMouseDown()
+    {
+        Prepos = Input.mousePosition;  //按下時先儲存初始位置
     }
 
-    public void OnMouseDrag(){
-        RectTransform rect = GetComponent<RectTransform>() ;
-        Vector2 temp = new Vector2(Input.mousePosition.x - Prepos.x, 
+    public void OnMouseDrag()
+    {
+        RectTransform rect = GetComponent<RectTransform>();
+        Vector2 temp = new Vector2(Input.mousePosition.x - Prepos.x,
                 Input.mousePosition.y - Prepos.y);// 取得RectTransform
-       
-        rect.anchoredPosition += (temp) ;     // 移動
-        Prepos = Input.mousePosition ;        // 重設先前位置
+        if (MainCitySys.Instance.MainCanvas != null && MainCitySys.Instance.MainCanvas.worldCamera != null)
+        {
+            rect.anchoredPosition += (temp);     // 移動
+            Prepos = Input.mousePosition;        // 重設先前位置
+            Camera maincam = MainCitySys.Instance.MainCanvas.worldCamera;
+            Vector2 StartPos = maincam.ViewportToWorldPoint(new Vector2(0, 0));//左下
+            Vector2 EndPos = maincam.ViewportToWorldPoint(new Vector2(1, 1));//右上
+            if ((rect.position.x + (rect.rect.width / 2)) > EndPos.x)
+            {
+                rect.position = new Vector2(EndPos.x - (rect.rect.width / 2), rect.position.y);
+            }
+            if ((rect.position.y + (rect.rect.height / 2)) > EndPos.y)
+            {
+                rect.position = new Vector2(rect.position.x, EndPos.y - (rect.rect.height / 2));
+            }
+            if ((rect.position.x - (rect.rect.width / 2)) < StartPos.x)
+            {
+                rect.position = new Vector2(StartPos.x + (rect.rect.width / 2), rect.position.y);
+            }
+            if ((rect.position.y - (rect.rect.height / 2)) < StartPos.y)
+            {
+                rect.position = new Vector2(rect.position.x, StartPos.y + (rect.rect.height / 2));
+            }
+        }
+
+
     }
 }
--- Assets/Scripts/Utility/ToolTip.cs
+++ Assets/Scripts/Utility/ToolTip.cs
@@ -8,12 +8,12 @@ public class ToolTip : MonoBehaviour
     public Text toolTipText;
     public Text contentText;
     public CanvasGroup canvasGroup;
-    
+
     private float targetAlpha = 0;
 
     public float smoothing = 3;
 
-    
+
 
     void Update()
     {
@@ -32,6 +32,44 @@ public class ToolTip : MonoBehaviour
         toolTipText.text = text;
         contentText.text = text;
         targetAlpha = 1;
+        LayoutRebuilder.ForceRebuildLayoutImmediate(transform.GetComponent<RectTransform>());
+        RectTransform rect = GetComponent<RectTransform>();
+        if (MainCitySys.Instance.MainCanvas != null && MainCitySys.Instance.MainCanvas.worldCamera != null)
+        {
+            //Camera.main.ScreenPointToRay(Input.mousePosition)
+            Camera maincam = MainCitySys.Instance.MainCanvas.worldCamera;
+            Vector2 mousepos = maincam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 StartPos = maincam.ViewportToWorldPoint(new Vector2(0, 0));//左下
+            Vector2 EndPos = maincam.ViewportToWorldPoint(new Vector2(1, 1));//右上
+
+            float offset = 3f;
+            //ToolTip 的Pivot在右下角
+            if (mousepos.x > (StartPos.x + EndPos.x) / 2) //在右邊
+            {
+                rect.position = new Vector2(mousepos.x - offset, mousepos.y + offset);
+            }
+            if (mousepos.x < (StartPos.x + EndPos.x) / 2) //在左邊
+            {
+                rect.position = new Vector2(mousepos.x + offset + rect.rect.width, mousepos.y + offset);
+            }
+            //判斷有沒有出界，移回畫面內
+            if (rect.position.x > EndPos.x)
+            {
+                rect.position = new Vector2(EndPos.x - offset, rect.position.y);
+            }
+            if ((rect.position.y + rect.rect.height) > EndPos.y)
+            {
+                rect.position = new Vector2(rect.position.x, EndPos.y - rect.rect.height);
+            }
+            if ((rect.position.x - rect.rect.width) < StartPos.x)
+            {
+                rect.position = new Vector2(StartPos.x + rect.rect.width + offset, rect.position.y);
+            }
+            if ((rect.position.y - rect.rect.height) < StartPos.y)
+            {
+                rect.position = new Vector2(rect.position.x, StartPos.y + rect.rect.height);
+            }
+        }
     }
     public void Hide()
     {
